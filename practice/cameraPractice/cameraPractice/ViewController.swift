315cc692b4deeafdaadf1f31b3305f4461813071
Mondale Felix <mondalefelix@yahoo.com>
2017-11-05 10:58:42 -0800

    added questions data model

--- practice/cameraPractice/cameraPractice/ViewController.swift
+++ practice/cameraPractice/cameraPractice/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    let captureSession = AVCaptureSession()
+    var previewLayer:CALayer!
+    //  Displays video stream
+    var captureDevice: AVCaptureDevice!
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        prepareCamera()
     }
 
+    func prepareCamera(){
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        if let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices{
+            captureDevice = availableDevices.first
+            beginSession()
+        }
+    }
+    
+    func beginSession(){
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession.addInput(captureDeviceInput)
+        } catch{
+            print(error.localizedDescription)
+        }
+    }
+    
+    if  let previewLayer = AVCaptureVideoPreviewLayer(session: AVCaptureSession){
+        self.previewLayer = self.previewLayer
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.layer.frame
+        captureSession.startRunning()
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString): NSNumber(value: kCVPixelFormatType_32BGRA)]
+        dataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        if captureSession.canAddOutput(dataOutput){
+            captureSession.addOutput(dataOutput)
+        }
+        
+        AVCaptureSession.commitConfiguration()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
